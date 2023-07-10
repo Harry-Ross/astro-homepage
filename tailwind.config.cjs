cdb91058a94bf1f6ee46e2d5c3021f0ab79ff54a
Harry Ross <hazross@hotmail.com>
2023-07-10 15:33:46 +1000

    Added Nunito

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Nunito", "sans-serif"],
+				inter: ['Inter', 'sans-serif'],
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
