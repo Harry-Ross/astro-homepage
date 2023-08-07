7884d478016e51ebac164f3283358fbd00b5af76
Harry Ross <hazross@hotmail.com>
2023-08-07 19:55:38 +1000

    Added correct SEO descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Harry Ross - Developer';
+export const SITE_DESCRIPTION = "Hi I'm Harry - a full stack .NET developer from Sydney, Australia. I'm passionate about building great software and learning new things.";
