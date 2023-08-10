58db8739e35ee12b9c7aa1c0f490a8e7863500fe
Harry Ross <hazross@hotmail.com>
2023-08-10 18:57:42 +1000

    Added correct SEO values

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Harry Ross - Developer';
+export const SITE_TITLE = 'Harry Ross - Full Stack Developer';
 export const SITE_DESCRIPTION = "Hi I'm Harry - a full stack .NET developer from Sydney, Australia. I'm passionate about building great software and learning new things.";
+export const SITE_IMAGE = "/placeholder-social.jpg"
